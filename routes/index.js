3d0a46211a8f36060cf46d3454a80a6345ce903f
slava <slava.shklyar@gmail.com>
2013-02-02 23:14:13 +0200

    Server side behavior (load data and echo):  - added HTTP headers

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@
  * GET home page.
  */
 
+var return_body = function(body, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Encoding', 'utf-8');
+    res.setHeader('Content-Length', unescape(encodeURIComponent(body)).length);
+    res.send(body);
+};
+
 exports.index = function(req, res){
   res.render('index', { title: 'Hola-Java Script' });
 };
@@ -17,9 +24,9 @@ exports.save = function(req, res) {
 };
 
 exports.select_json = function(req, res) {
-    res.send('{"D":"Letter D","E":"Letter E","F":"Letter F","ג":"אות ג","selected":"F"}');
+    return_body('{"D":"Letter D","E":"Letter E","F":"Letter F","ג":"אות ג","selected":"F"}', res);
 };
 
 exports.load = function(req, res) {
-    res.send('הטלוויזיה הסורית פרסמה הערב (שבת) תמונות מהאתר ב"מרכז המחקר המדעי", שעל פי פרסומים זרים הותקף על ידי ישראל. בתמונות ובסרטון, ששודר בערוץ 10, נראים כלי רכב שנהרסו בהפגזה. הגרסה הסורית לגבי האתר שהופצץ עדיין שנויה במחלוקת, וייתכן שהם מנסים להסתיר את המקום שהיה היעד.');
+    return_body('הטלוויזיה הסורית פרסמה הערב (שבת) תמונות מהאתר ב"מרכז המחקר המדעי", שעל פי פרסומים זרים הותקף על ידי ישראל. בתמונות ובסרטון, ששודר בערוץ 10, נראים כלי רכב שנהרסו בהפגזה. הגרסה הסורית לגבי האתר שהופצץ עדיין שנויה במחלוקת, וייתכן שהם מנסים להסתיר את המקום שהיה היעד.', res);
 };
